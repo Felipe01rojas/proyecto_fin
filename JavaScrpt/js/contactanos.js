a92fa572000525cd98506abaf867015bf24eadf0
Felipe01rojas <kinrojas2@gmail.com>
2022-03-10 22:20:07 -0500

    nuevos archivos

--- JavaScrpt/js/contactanos.js
+++ JavaScrpt/js/contactanos.js
@@ -1,26 +1,22 @@
 'use strict'
 
 
-function validar(){
-    let nombre_usuario= document.getElementById('nombre_usuario').value;
-    if(nombre_usuario=== '');
+function validarInfo(){
+    let nombre_usuario= document.getElementById("nombre_usuario").value;
+    if(nombre_usuario === ""){
     alert('el nombre es obligatorio')
 }
 
-function validar(){
-    let apellido_usuario = document.getElementById('apellido_usuario').value;
-    if(apellido_usuario=== '')
+
+    let apellido_usuario = document.getElementById("apellido_usuario").value;
+    if(apellido_usuario === ""){
     alert ('el apellido es obligatorio')
 }
 
-function validar(){
-    let genero = document.getElementById('genero').value;
-    if(genero=== '')
-    alert ('el genero es obligatorio por favor seleccione su genero')
-}
-
-function validar(){
-    let ipqr = document.getElementById('ipqr').value;
-    if(ipqr=== '')
+    let consulta = document.getElementById("consulta").value;
+    if(consulta === ""){
     alert ('la petición, queja o reclamo son obligatorios')
+}else{
+    alert("Información valida")
+}
 }
